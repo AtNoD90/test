d3076f7bf073fc503465a5cb835908e70cc6c840
AtNoD90 <the.atnod@gmail.com>
2023-06-16 22:19:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 const elements = [
   {
     title: 'Element 1',
-    image: 'image1.jpg',
+    image: 'logo.PNG',
     links: [
-      { text: 'Link 1.1', url: 'https://example.com/link1.1' },
-      { text: 'Link 1.2', url: 'https://example.com/link1.2' },
-      { text: 'Link 1.3', url: 'https://example.com/link1.3' }
+      { text: 'Link 1.1', url: 'https://atnod90.github.io/test' },
+      { text: 'Link 1.2', url: 'https://atnod90.github.io/test' },
+      { text: 'Link 1.3', url: 'https://atnod90.github.io/test' }
     ],
-    imageLink: 'https://example.com/image1'
+    imageLink: 'https://atnod90.github.io/test'
   },
   {
     title: 'Element 2',
-    image: 'image2.jpg',
+    image: 'logo.PNG',
     links: [
-      { text: 'Link 2.1', url: 'https://example.com/link2.1' },
-      { text: 'Link 2.2', url: 'https://example.com/link2.2' },
-      { text: 'Link 2.3', url: 'https://example.com/link2.3' }
+      { text: 'Link 2.1', url: 'https://atnod90.github.io/test' },
+      { text: 'Link 2.2', url: 'https://atnod90.github.io/test' },
+      { text: 'Link 2.3', url: 'https://atnod90.github.io/test' }
     ],
-    imageLink: 'https://example.com/image2'
+    imageLink: 'https://atnod90.github.io/test'
   },
   // Add more elements as needed
 ];
